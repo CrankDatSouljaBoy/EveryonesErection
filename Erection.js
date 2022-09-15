57094eb713625296c32136c34ed4545db72e9bbb
CrankDatSouljaBoy <75489748+CrankDatSouljaBoy@users.noreply.github.com>
2022-09-15 04:41:27 +0100

    why

--- Erection.js
+++ Erection.js
@@ -14,18 +14,18 @@ module.exports = {
         // this might be really inefficient, but it only runs once each reload
         for (let i = 1926; i < 9999; i++) {
             const pageString = `00${i}`;
-			// if the page exists (prevents certain errors)
+	    // if the page exists (prevents certain errors)
             if (archive.mspa.story[pageString]) {
                 const content = archive.mspa.story[pageString].content;
-				// store all of EVERYoneesn's lines
+                // store all of EVERYoneesn's lines
                 const lines = [];
                 while(searchMatch = searchRegex.exec(content)) lines.push(searchMatch[2]);
                 for (let x = 0; x < 4; x++) {
-					// choose a random one
-					const selectedText = lines[Math.floor(Math.random() * lines.length)];
-					// replace chosen line
-					archive.mspa.story[pageString].content = content.replace(selectedText, 'i have an erection.');
-				}
+		    // choose a random one
+		    const selectedText = lines[Math.floor(Math.random() * lines.length)];
+		    // replace chosen line
+		    archive.mspa.story[pageString].content = content.replace(selectedText, 'i have an erection.');
+		}
             }
         }
     },
